2004ced4bc1cd7532c834af38c392ec8d960d406
barretotheo25 <83764653+barretotheo25@users.noreply.github.com>
2021-06-08 22:49:24 -0300

    Update valor_n#2.sh

--- FundamentosTI/exemplos/valor_n#2.sh
+++ FundamentosTI/exemplos/valor_n#2.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 # autor Théo Barreto Silva
-# descrição 
+# descrição Esse script vai pedir ao usuário um valor N e irá imprimir todos os valores entre 1 e N, porém ele só vai aceitar valores maiores que 0.
+
+echo "Digite um valor"
+read N
+  if [ $N -gt 0 ]
+  then
+  
+    for i in $(seq 1 $N)
+    do
+      echo "- $i"
+    done
+else
+    echo "Digitem um valo maior que zero"
+fi
