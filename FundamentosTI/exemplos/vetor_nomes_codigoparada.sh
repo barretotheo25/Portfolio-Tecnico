84283565126cb8ab1ea9f4afe62599e0bfc548e7
barretotheo25 <83764653+barretotheo25@users.noreply.github.com>
2021-06-11 15:43:30 -0300

    Create vetor_nomes_codigoparada.sh

---
+++ FundamentosTI/exemplos/vetor_nomes_codigoparada.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# autor Théo Barreto Silva
+# descrição Esse script vai edir para o usuário nomes de pessoas utilizando loop. Ele vai parar de pedir a próxima pessoa quando o usuário digitar um “código de parada”. No final vai dizer quantidade de pessoas digitadas.
+
+cod_parada="exit"
+nome=""
+i=0
+echo "Se quiser sair, usar exit"
+echo
+while [[ $nome != $cod_parada ]]
+do
+  echo "Escreva um nome:"
+  read nome
+  if [[ $nome != $cod_parada ]]
+  then
+    vetor[$i]=$nome
+    i=$((i+1))
+    echo "O nome escolhido foi $nome"
+  fi
+done
+echo "Tamanho do vetor: ${#vetor[@]}"
