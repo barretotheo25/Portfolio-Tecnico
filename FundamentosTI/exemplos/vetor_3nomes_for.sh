fe69e142f31663a018d21a826f3b5e07df1fe187
barretotheo25 <83764653+barretotheo25@users.noreply.github.com>
2021-06-11 15:21:45 -0300

    Update vetor_3nomes_for.sh

--- FundamentosTI/exemplos/vetor_3nomes_for.sh
+++ FundamentosTI/exemplos/vetor_3nomes_for.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # autor Théo Barreto Silva
-# descrição Esse script vai fazer um loop com 3 nomes, utilizando o for, ele vai imprimir "Oi, eu sou ____"
+# descrição Esse script vai fazer um loop com 3 nomes, utilizando o for, ele vai imprimir "Oi, eu sou ____".
 vetor[0]="Théo"
 vetor[1]="Ana"
 vetor[2]="Sophia"
