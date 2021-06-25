eb986e45f6daa74327baa458f4043dcdc633a42b
barretotheo25 <83764653+barretotheo25@users.noreply.github.com>
2021-06-25 08:29:22 -0300

    Update ordem_crescente.sh

--- FundamentosTI/exemplos/ordem_crescente.sh
+++ FundamentosTI/exemplos/ordem_crescente.sh
@@ -2,7 +2,7 @@
 # autor Théo Barreto Silva
 # descrição Esse script vai imprimir os números de 1 a 10 em ordem crescente.
 i=1
-while [$i -lt 11 ]
+while [ $i -lt 11 ]
 do
   echo "- $i"
   i=$((i+1))
