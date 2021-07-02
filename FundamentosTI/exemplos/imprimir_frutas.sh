80c7200a5d94105f37e7772c0ecd28e1d532b4e1
barretotheo25 <83764653+barretotheo25@users.noreply.github.com>
2021-07-02 08:38:10 -0300

    Update imprimir_frutas.sh

--- FundamentosTI/exemplos/imprimir_frutas.sh
+++ FundamentosTI/exemplos/imprimir_frutas.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # autor Théo Barreto Silva
 # descrição Esse script vai imprimir apenas a lista de frutas, sem os números de ordem nem os preços.
-cd ..  # usei o “cd ..” pois minha lista de frutas estava em um diretório de fora
-cd ..  # usei o “cd ..” pois minha lista de frutas estava em um diretório de fora
 lista_frutas=$(cut -d" " -f2 lista_frutas)
 echo "$lista_frutas"
