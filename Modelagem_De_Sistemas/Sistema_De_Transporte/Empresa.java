7e035a3436058b23c89dff5162047b2a8f149eac
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:13:57 -0300

    Update Empresa.java

--- Modelagem_De_Sistemas/Sistema_De_Transporte/Empresa.java
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Empresa.java
@@ -1,1 +1,31 @@
+public class Empresa{
+  private String razaoSocial;
+  private String nomeFantasia;
+  private String cnpj;
+  private String endereco;
+  private String telefone;
+  private String email;
+  private String despesas;
 
+  public Empresa(String razaoSocial, String nomeFantasia, String cnpj, String endereco, String telefone, String email, String despesas){
+    this.razaoSocial = razaoSocial;
+    this.nomeFantasia = nomeFantasia;
+    this.cnpj = cnpj;
+    this.endereco = endereco;
+    this.telefone = telefone;
+    this.email = email;
+    this.despesas = despesas;
+  }
+
+  public boolean contratarMotorista(String pis, String salario){
+    return false;
+  }
+
+  public boolean pagarFuncionario(String salario, String pis){
+    return true;
+  }
+
+  public boolean contratarFuncionario(String nome, String salario, String pis){
+    return false;
+  }
+}
