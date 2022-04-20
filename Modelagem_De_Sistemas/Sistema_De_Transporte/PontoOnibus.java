75a779c8874ca6c12355de61be4288af7a857dfe
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:13:18 -0300

    Create PontoOnibus.java

---
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/PontoOnibus.java
@@ -0,0 +1,11 @@
+public class PontoOnibus{
+  private String endereco;
+  private Onibus onibus;
+  private Passageiro passageiro;
+
+  public PontoOnibus(String endereco, Onibus onibus, Passageiro passageiro){
+    this.endereco = endereco;
+    this.onibus = onibus;
+    this.passageiro = passageiro;
+  }
+}
