4364f7025e2b2d9115a5f9c571e8cd65fce1aa85
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:31:51 -0300

    Update Passageiro.java

--- Modelagem_De_Sistemas/Sistema_De_Transporte/Passageiro.java
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Passageiro.java
@@ -1,1 +1,30 @@
+public class Passageiro extends Pessoa{
+  private String enderecoAtual;
+  private String enderecosFavoritos;
+  private String horarios;
+  private String empresasFavoritas;
 
+  private Passageiro(String nome, String telefone, String email, char genero, String cpf, String data_nascimento, String endereco, String enderecoAtual, String enderecosFavoritos, String horarios, String empresasFavoritas){
+    super(nome, telefone, email, genero, cpf, data_nascimento, endereco);
+    this.enderecoAtual = enderecoAtual;
+    this.enderecosFavoritos = enderecosFavoritos;
+    this.horarios = horarios;
+    this.empresasFavoritas = empresasFavoritas;
+  }
+
+  private void cadastroPassageiro(String enderecosFavoritos, String horarios, String empresasFavoritas){
+    
+  }
+
+  private boolean esperarOnibus(String enderecoAtual, String horarios, String empresasFavoritas){
+    return false;
+  }
+
+  private void pegarOnibus(String enderecoAtual, String horarios){
+    
+  }
+
+  private void descerDoOnibus(PontoOnibus pontoOnibus){
+    
+  }
+}
