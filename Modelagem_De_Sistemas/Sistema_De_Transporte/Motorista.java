7b97a0b688126ff9db2e2e4d99a6423c0a4959ef
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:31:14 -0300

    Update Motorista.java

--- Modelagem_De_Sistemas/Sistema_De_Transporte/Motorista.java
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Motorista.java
@@ -1,1 +1,38 @@
+public class Motorista extends Pessoa{
+  private String horarioDeTrabalho;
+  private Empresa empresa;
+  private String salario;
+  private String pis;
+  private String funcao;
+  private boolean bateuPonto;
 
+  public Motorista (String nome, String telefone, String email, char genero, String cpf, String data_nascimento, String endereco, String horarioDeTrabalho, Empresa empresa, String salario, String pis, String funcao, boolean bateuPonto){
+    super(nome, telefone, email, genero, cpf, data_nascimento, endereco);
+    this.horarioDeTrabalho = horarioDeTrabalho;
+    this.empresa = empresa;
+    this.salario = salario;
+    this.pis = pis;
+    this.funcao = funcao;
+    this.bateuPonto = bateuPonto;
+  }
+
+  public void cadastroMotorista(String horarioDeTrabalho, Empresa empresa, String pis){
+    
+  }
+
+  public void irTrabalhar(String horarioDeTrabalho, Empresa empresa, Onibus onibus){
+    
+  }
+
+  public boolean fazerRotaOnibus(Rotas rotas, Onibus onibus, PontoOnibus pontoOnibus){
+    return true;
+  }
+
+  public void baterPonto(String horarioDeTrabalho, String pis){
+    
+  }
+
+  public void receberSalario(String pis){
+    
+  }
+}
