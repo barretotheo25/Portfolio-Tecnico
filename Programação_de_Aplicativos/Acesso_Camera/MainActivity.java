89a8f8fe037d3fa226d13ae7bc9c08f1ae68c3cc
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:24:05 -0300

    Create MainActivity.java

---
+++ Programação_de_Aplicativos/Acesso_Camera/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.acesso_camera;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imagem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        imagem = findViewById(R.id.iv); // 'R' = representa a parta 'res' que tem todos os "designs"
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        }
+    }
+    public void tiraFoto(View blabla){
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap foto = (Bitmap) extras.get("data");
+            imagem.setImageBitmap(foto);
+        }
+    }
+}
