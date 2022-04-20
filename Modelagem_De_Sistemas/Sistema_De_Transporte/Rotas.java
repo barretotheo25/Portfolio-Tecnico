0892013a9dee5d5cfaebc6ebe40a0438d7b3dfd7
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:13:35 -0300

    Create Rotas.java

---
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Rotas.java
@@ -0,0 +1,9 @@
+public class Rotas{
+  private Onibus onibus;
+  private PontoOnibus pontoOnibus;
+
+  public Rotas(Onibus onibus, PontoOnibus pontoOnibus){
+    this.onibus = onibus;
+    this.pontoOnibus = pontoOnibus;
+  }
+}
