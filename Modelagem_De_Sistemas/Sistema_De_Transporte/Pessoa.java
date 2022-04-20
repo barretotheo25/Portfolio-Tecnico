33311ca6eb27b9cea6b47ea50b55ec2b8499c8c3
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:32:09 -0300

    Update Pessoa.java

--- Modelagem_De_Sistemas/Sistema_De_Transporte/Pessoa.java
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Pessoa.java
@@ -1,1 +1,27 @@
+public class Pessoa{
+  private String nome;
+  private String telefone;
+  private String email;
+  private char genero;
+  private String cpf;
+  private String data_nascimento;
+  private String endereco;
 
+  public Pessoa(String nome, String telefone, String email, char genero, String cpf, String data_nascimento, String endereco){
+    this.nome = nome;
+    this.telefone = telefone;
+    this.email = email;
+    this.genero = genero;
+    this.cpf = cpf;
+    this.data_nascimento = data_nascimento;
+    this.endereco = endereco;
+  }
+
+  public boolean baixarAplicativo (){
+    return true;
+  }
+
+  public void cadastroBasicoApp (String nome, String telefone, String email, char genero, String cpf, String data_nascimento, String endereco){
+    
+  }
+}
