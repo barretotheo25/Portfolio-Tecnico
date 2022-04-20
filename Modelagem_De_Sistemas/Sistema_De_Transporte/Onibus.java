f5e73f4bfe7172f1f3450038ab1d326d25b7d14f
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-20 11:31:33 -0300

    Update Onibus.java

--- Modelagem_De_Sistemas/Sistema_De_Transporte/Onibus.java
+++ Modelagem_De_Sistemas/Sistema_De_Transporte/Onibus.java
@@ -1,1 +1,17 @@
+public class Onibus{
+  private String marca;
+  private boolean articulado;
+  private Empresa empresa;
+  private int numeroIdentificacao;
 
+  public Onibus(String marca, boolean articulado, Empresa empresa, int numeroIdentificacao){
+    this.marca = marca;
+    this.articulado = articulado;
+    this.empresa = empresa;
+    this.numeroIdentificacao = numeroIdentificacao;
+  }
+
+  public void levarPassageiros(Passageiro passageiro, Motorista motorista){
+    
+  }
+}
