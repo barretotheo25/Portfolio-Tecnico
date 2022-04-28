65b21c9957a9a33438f3808d005673c77311811f
Théo <83764653+barretotheo25@users.noreply.github.com>
2022-04-28 11:08:53 -0300

    Create Bolo.java

---
+++ Modelagem_De_Sistemas/Confeitaria-Bolo.java/Bolo.java
@@ -0,0 +1,14 @@
+public class Bolo{
+  private String recheio;
+  private String massa;
+  private String cobertura;
+  private boolean decoracao;
+
+  public Bolo(String recheio, String massa, String   
+  cobertura, boolean decoracao){
+    this.recheio = recheio;
+    this.massa = massa;
+    this.cobertura = cobertura;
+    this.decoracao = decoracao;
+  }
+}
